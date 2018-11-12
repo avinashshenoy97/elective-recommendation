b7a38aeecfafcebbefe03b51a3d22be457952569
Avinash Shenoy <avi123shenoy@hotmail.com>
2018-11-12 23:04:29 +0530

    Added autocomplete

--- ui/public/js/home.js
+++ ui/public/js/home.js
@@ -2,12 +2,46 @@ function HomeService($http) {
     var self = this;
     self.http = $http;
 
+    self.autoCompleteUrl = '/idk';
+
+    self.autoCompleteList = [];
+
+    self.getAutoCompleteList = function() {
+        self.http.get(self.autoCompleteUrl).then(
+            function success(response) {
+                self.autoCompleteList = response.data;
+            },
+            function error(response) {
+                // self.autoCompleteList = ['abc', 'def', 'ghi']; //to test
+                console.log('Error fetching autocomplete list');
+                console.log(response);
+            }
+        )
+    };
 }
 
 function HomeController(loginService, homeService) {
     var self = this;
     self.homeService = homeService;
     self.loginService = loginService;
+
+    self.homeService.getAutoCompleteList();
+    self.input = '';
+    self.autoCompleteList = [];
+
+    self.autoCompleter = function() {
+        self.autoCompleteList = [];
+
+        if(self.input.length == 0) {
+            return;
+        }
+
+        self.homeService.autoCompleteList.forEach(element => {
+            if(element.match(self.input)) {
+                self.autoCompleteList.push(element);
+            }
+        });
+    };
 
 }
 
